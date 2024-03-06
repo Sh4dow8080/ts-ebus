93f633f85e965165416c930cda4aa86c50245c94
Sh4dow <sh4dowx8080@gmail.com>
2024-03-06 08:26:22 +0100

    Added event middleware support.

--- src/lib/EBus.ts
+++ src/lib/EBus.ts
@@ -8,17 +8,27 @@ type Class = { new(...args: any): any };
 type InstanceOfClass = InstanceType<Class>;
 type Unsubscribe = () => void;
 type Listener<TEvent extends Class> = (event: InstanceType<TEvent>) => void;
+type NoInfer<T> = [T][T extends any ? 0 : never];
+
 interface IEBus {
 	on<TEvent extends Class>(
 		event: TEvent,
 		listener: (event: TEvent) => void
 	): Unsubscribe;
 	send<TEvent extends InstanceOfClass>(event: TEvent): void;
+	registerMiddleware<
+		TEvent extends Class,
+		TEventInstance extends InstanceType<NoInfer<TEvent>>
+	>(
+		event: TEvent,
+		middleware: (event: TEventInstance) => TEventInstance
+	): Unsubscribe;
 }
 
 export class EBus implements IEBus {
 	private currentEventId: number = 0;
 	private subscribers: Subscriber = {};
+	private middlewares: { [eventName: string]: Function[] } = {};
 
 	public on<TEvent extends Class>(
 		event: TEvent,
@@ -41,13 +51,41 @@ export class EBus implements IEBus {
 			return;
 		}
 
+		const middlewares = this.middlewares[eventName] || [];
+		let currentEvent = event;
+		for (const middleware of middlewares) {
+			currentEvent = middleware(currentEvent);
+		}
+
 		const eventSubscribers = this.subscribers[eventName];
 		const eventIds = Object.keys(eventSubscribers);
 		eventIds.forEach((eventId) => {
 			const listener = eventSubscribers[eventId];
-			listener(event);
+			listener(currentEvent);
 		});
 	}
+
+	public registerMiddleware<
+		TEvent extends Class,
+		TEventInstance extends InstanceType<NoInfer<TEvent>>
+	>(
+		event: TEvent,
+		middleware: (event: TEventInstance) => TEventInstance
+	): Unsubscribe {
+		const eventName = event.prototype.constructor.name;
+		if (!this.middlewares[eventName]) {
+			this.middlewares[eventName] = [];
+		}
+
+		this.middlewares[eventName].push(middleware);
+
+		return () => {
+			this.middlewares[eventName] = this.middlewares[eventName].filter(
+				(m) => m !== middleware
+			);
+		};
+	}
+
 
 	private getNextEventId() {
 		return this.currentEventId++;
--- tests/ebus.test.ts
+++ tests/ebus.test.ts
@@ -2,30 +2,33 @@ import { EBus } from "../src";
 
 let bus = new EBus();
 beforeEach(() => {
-	bus = new EBus();
+    bus = new EBus();
 });
 
-class TestEvent {}
-class TestEvent2 {}
+class TestEvent { }
+class TestEvent2 { }
+class TestEvent3 {
+    constructor(public value: string) { }
+}
 
 test("EBus on<...> should return an unsubscribe function", () => {
     // Arrange
-	const unsubscribe = bus.on(TestEvent, () => {});
+    const unsubscribe = bus.on(TestEvent, () => { });
 
     // Assert
-	expect(unsubscribe).toBeInstanceOf(Function);
+    expect(unsubscribe).toBeInstanceOf(Function);
 });
 
 test("EBus on<...> should call listener when send<...> is called", () => {
-	// Arrange
-	const listener = jest.fn();
-	bus.on(TestEvent, listener);
+    // Arrange
+    const listener = jest.fn();
+    bus.on(TestEvent, listener);
 
-	// Act
-	bus.send(new TestEvent());
+    // Act
+    bus.send(new TestEvent());
 
-	// Assert
-	expect(listener).toHaveBeenCalled();
+    // Assert
+    expect(listener).toHaveBeenCalled();
 });
 
 test("EBus on<...> should not call listener after unsubscribe is called", () => {
@@ -51,4 +54,24 @@ test("EBus on<...> should call listener for the correct event", () => {
 
     // Assert
     expect(listener).not.toHaveBeenCalled();
+});
+
+test("EBus on<...> should call listener with the correct event when using middleware", () => {
+    // Arrange
+    const listener = jest.fn();
+    const middleware = jest.fn((event: TestEvent3) => {
+        return new TestEvent3("fisk");
+    });
+
+    bus.registerMiddleware(TestEvent3, middleware);
+    bus.on(TestEvent3, listener);
+
+    // Act
+    bus.send(new TestEvent3("kat"));
+
+    // Assert
+    expect(middleware).toHaveBeenCalled();
+    expect(middleware).toHaveBeenCalledWith(new TestEvent3("kat"));
+
+    expect(listener).toHaveBeenCalledWith(new TestEvent3("fisk"));
 });
